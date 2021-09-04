2c942877f4764aa0ccfbcf85733c71ed9804afe8
Hendrick <sebastinesoacatp@gmail.com>
2021-09-04 22:22:33 +0100

    practice

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,81 @@ class HelloBooksApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Books'),
-        ),
-        body: Center(
-          child: Text(
-            'Hello Books',
-            style: Theme.of(context).textTheme.headline4,
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Hello Books'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> greetings = [
+    'Hello Books',
+    'Hola Libros',
+    'Ciao Libri',
+    'ैलो िकताब'
+  ];
+
+  int index = 0;
+  late String current;
+  void _updateGreeting() {
+    setState(() {
+      current = greetings[index];
+      index = index == (greetings.length - 1) ? 0 : index + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text(widget.title),
+    //   ),
+    //   body: Center(
+    //     child: Text(
+    //       greetings[index],
+    //       style: Theme.of(context).textTheme.headline4,
+    //     ),
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: _updateGreeting,
+    //     tooltip: 'Greeting',
+    //     child: Icon(Icons.insert_emoticon),
+    //   ),
+    // );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Image Widget"),
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 300,
+          padding: const EdgeInsets.all(20.0),
+          child: ToggleButtons(
+            children: [
+              Icon(Icons.airplanemode_off),
+              Icon(Icons.airplanemode_on),
+            ],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          tooltip: 'Greeting',
-          child: Icon(Icons.insert_emoticon),
         ),
       ),
     );
+  }
+
+  Widget loadLocalImage() {
+    return Image.asset("assets/flutter.png");
+  }
+
+  Widget loadInternetImage() {
+    return Image.network(
+        "https://github.com/ptyagicodecamp/flutter_cookbook2/raw/master/assets/flutter_icon.png");
   }
 }
