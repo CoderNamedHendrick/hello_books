1d098a9a2ed65915b4e507ab2e20b823bbec8bac
Hendrick <sebastinesoacatp@gmail.com>
2021-09-05 02:51:40 +0100

    dealing with dark and light mode themes

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,21 @@ void main() {
   runApp(BooksApp());
 }
 
-class BooksApp extends StatelessWidget {
+enum AppThemes { LIGHT, DARK }
+
+class BooksApp extends StatefulWidget {
+  @override
+  _BooksAppState createState() => _BooksAppState();
+}
+
+class _BooksAppState extends State<BooksApp> {
+  var currentTheme = AppThemes.LIGHT;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: defaultTheme,
+      debugShowCheckedModeBanner: false,
+      theme: currentTheme == AppThemes.LIGHT ? defaultTheme : darkTheme,
       home: Scaffold(
         appBar: AppBar(
           title: Text(
@@ -19,6 +29,20 @@ class BooksApp extends StatelessWidget {
               fontSize: 30,
             ),
           ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(
+                  () {
+                    currentTheme = currentTheme == AppThemes.LIGHT
+                        ? AppThemes.DARK
+                        : AppThemes.LIGHT;
+                  },
+                );
+              },
+              icon: Icon(Icons.all_inclusive),
+            ),
+          ],
         ),
         body: BooksListing(),
       ),
@@ -30,51 +54,65 @@ class BooksListing extends StatelessWidget {
   final booksListing = bookData();
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: booksListing.length,
-      itemBuilder: (context, index) {
-        return Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          elevation: 5,
-          margin: EdgeInsets.all(10),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '${booksListing[index]['title']}',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      booksListing[index]['authors'] != null
-                          ? Text(
-                              'Author(s): ${booksListing[index]['authors'].join(", ")}',
-                              style: TextStyle(
-                                fontSize: 14,
-                              ),
-                            )
-                          : Text(""),
-                    ],
-                  ),
+    return Theme(
+      data: ThemeData(
+        cardColor: Colors.pinkAccent,
+        textTheme: TextTheme(
+          bodyText2: Theme.of(context)
+              .copyWith(
+                textTheme: TextTheme(
+                  bodyText2: TextStyle(fontStyle: FontStyle.italic),
                 ),
-                Image.asset(
-                  booksListing[index]['image'],
-                  fit: BoxFit.fill,
-                )
-              ],
-            ),
+              )
+              .textTheme
+              .bodyText2,
+          headline6: TextStyle(
+            fontFamily: 'Pangolin',
+            fontSize: 20,
           ),
-        );
-      },
+        ),
+      ),
+      child: ListView.builder(
+        itemCount: booksListing.length,
+        itemBuilder: (context, index) {
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 5,
+            margin: EdgeInsets.all(10),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${booksListing[index]['title']}',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        booksListing[index]['authors'] != null
+                            ? Text(
+                                'Author(s): ${booksListing[index]['authors'].join(", ")}',
+                                style: Theme.of(context).textTheme.bodyText2,
+                              )
+                            : Text(""),
+                      ],
+                    ),
+                  ),
+                  Image.asset(
+                    booksListing[index]['image'],
+                    fit: BoxFit.fill,
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
